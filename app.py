4e42e54c3a2e1c6b5e7c13d7c90839b6835a0eee
parth060705 <parth.gharat@openlanesolutions.com>
2025-05-27 10:53:34 +0530

    commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///passwordsaver.db" # to create database
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db=SQLAlchemy(app)
+
+# -------------------------------------------------------------------------------------------
+# define the model
+class passwordsaver(db.Model):
+    sno = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    website = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<passwordsaver {self.sno}>" # for print the specific data
+# --------------------------------------------------------------------------------------------
+
+@app.route('/')    
+def index():
+    allsavepassword=passwordsaver.query.all()
+    return render_template('index.html', allsavepassword=allsavepassword) # to render template file
+
+# to post the credential to database
+@app.route('/', methods=['POST'])
+def register():
+    website = request.form["website"]
+    email = request.form["email"]
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    new_website = passwordsaver(website=website, email=email, username=username, password=password)
+    db.session.add(new_website) # to add the data 
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:sno>', methods=['GET'])
+def delete(sno):
+    delete_savepassword = passwordsaver.query.filter_by(sno=sno).first()
+    if delete_savepassword:
+        db.session.delete(delete_savepassword) # to delete the data
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:sno>', methods=['GET'])
+def update_form(sno):
+    update_savepassword = passwordsaver.query.filter_by(sno=sno).first()
+    if update_savepassword:
+        return render_template('update.html', data = update_savepassword) # to render template file and switch to the update page
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:sno>', methods=['POST'])# update the credentials
+def update(sno):
+    update_savepassword = passwordsaver.query.filter_by(sno=sno).first()
+    if update_savepassword:
+        update_savepassword.email = request.form["email"]
+        update_savepassword.username = request.form["username"]
+        update_savepassword.password = request.form["password"]
+        db.session.commit()
+    return redirect(url_for('index'))
+
+
+# to start the app
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=8000)
